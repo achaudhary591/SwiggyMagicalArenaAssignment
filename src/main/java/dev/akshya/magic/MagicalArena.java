390c9e6e08f7cb53a4eb971ba4449684b7b2673c
achaudhary591 <achaudhary591@gmail.com>
2024-06-28 20:25:46 +0530

    main method finished

--- src/main/java/dev/akshya/magic/MagicalArena.java
+++ src/main/java/dev/akshya/magic/MagicalArena.java
@@ -1,7 +1,15 @@
 package dev.akshya.magic;
 
+import dev.akshya.magic.model.Arena;
+import dev.akshya.magic.model.DuelArena;
+import dev.akshya.magic.model.IPlayer;
+import dev.akshya.magic.model.Player;
+
 public class MagicalArena {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        IPlayer playerA = new Player("A", 20, 2, 2);
+        IPlayer playerB = new Player("B", 20, 1, 3);
+        Arena duelArena = new DuelArena("Arena1", playerA, playerB);
+        duelArena.startGame();
     }
 }
